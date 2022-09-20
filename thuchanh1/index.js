15045b2b63fee490b38718e7a4ce37da6c16da28
minhduc22799 <minhduc22799@gmail.com>
2022-09-20 09:49:26 +0700

    commit thu hanh tao va goi ham

---
+++ thuchanh1/index.js
@@ -0,0 +1,11 @@
+function square(number){
+    var result = number * number;
+    return result;
+}
+
+function calculate(){
+    var input = document.getElementById("input").value;
+    var result= square(input);
+    document.getElementById("result").innerHTML = result;
+}
+
